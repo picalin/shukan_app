9c3b0193e183a41e1cadf821082b8e8593b21962
picalin <s.hikaru.2125@gmail.com>
2021-12-03 21:20:33 +0900

    set the routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks',
+    # :mailer => 'users/mailer',
+  }
+
+  devise_scope :user do
+    root :to => "users/sessions#new"
+    get "signup", :to => "users/sessions#new"
+    get "login", :to => "users/sessions#new"
+    delete "logout", :to => "users/sessions#destroy"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
