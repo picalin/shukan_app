619f2776c357c995c976251839eb5fdca485c1c2
picalin <s.hikaru.2125@gmail.com>
2021-12-05 14:27:02 +0900

    minor change

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -19,9 +19,9 @@ class Users::SessionsController < Devise::SessionsController
   # end
   
   # ログイン後にトップページへ遷移させる。
-  # def after_sign_in_path_for(user)
-  #   top_path
-  # end
+  def after_sign_in_path_for(user)
+    login_path
+  end
   
   def after_sign_out_path_for(user)
     root_path
