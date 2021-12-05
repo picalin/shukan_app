84ed3bfdbfa41a639e461374544b8659e359041f
picalin <s.hikaru.2125@gmail.com>
2021-12-05 15:49:31 +0900

    put logout function

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -20,7 +20,7 @@ class Users::SessionsController < Devise::SessionsController
   
   # ログイン後にトップページへ遷移させる。
   def after_sign_in_path_for(user)
-    login_path
+    top_index_path
   end
   
   def after_sign_out_path_for(user)
