1a94d0b05abbb3d8784e9de5cd8f60200cc965fd
picalin <s.hikaru.2125@gmail.com>
2021-12-06 22:51:03 +0900

    edit func for profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -53,6 +53,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :email, :password, :password_confirmation])
   end
+  
+  #パスワードを入力せずに登録情報をアップデート
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
