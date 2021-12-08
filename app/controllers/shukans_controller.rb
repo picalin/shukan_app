30c7c06aca97187a484123a75ab7f2ecb7723720
picalin <s.hikaru.2125@gmail.com>
2021-12-08 22:23:46 +0900

    updated shukans_controller

--- app/controllers/shukans_controller.rb
+++ app/controllers/shukans_controller.rb
@@ -4,16 +4,17 @@ class ShukansController < ApplicationController
 
   # GET /shukans or /shukans.json
   def index
-    @shukans = Shukan.all
+    #current_userのshukanをすべてとってくる
+    @shukans = current_user.shukan.all
   end
 
   # GET /shukans/1 or /shukans/1.json
-  def show
-  end
+  # def show
+  # end
 
   # GET /shukans/new
   def new
-    @shukan = Shukan.new
+    @shukan = current_user.shukan.new
   end
 
   # GET /shukans/1/edit
@@ -22,45 +23,43 @@ class ShukansController < ApplicationController
 
   # POST /shukans or /shukans.json
   def create
-    @shukan = Shukan.new(shukan_params)
+    @shukan = current_user.shukan.new(shukan_params)
 
-    respond_to do |format|
-      if @shukan.save
-        format.html { redirect_to @shukan, notice: "Shukan was successfully created." }
-        format.json { render :show, status: :created, location: @shukan }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @shukan.errors, status: :unprocessable_entity }
-      end
+    if @shukan.save
+      # format.html { redirect_to @shukan, notice: "Shukan was successfully created." }
+      # format.json { render :show, status: :created, location: @shukan }
+      @status = true
+    else
+      # format.html { render :new, status: :unprocessable_entity }
+      # format.json { render json: @shukan.errors, status: :unprocessable_entity }
+      @status = false
     end
   end
 
   # PATCH/PUT /shukans/1 or /shukans/1.json
   def update
-    respond_to do |format|
-      if @shukan.update(shukan_params)
-        format.html { redirect_to @shukan, notice: "Shukan was successfully updated." }
-        format.json { render :show, status: :ok, location: @shukan }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @shukan.errors, status: :unprocessable_entity }
-      end
+    if @shukan.update(shukan_params)
+      # format.html { redirect_to @shukan, notice: "Shukan was successfully updated." }
+      # format.json { render :show, status: :ok, location: @shukan }
+      @status = true
+    else
+      # format.html { render :edit, status: :unprocessable_entity }
+      # format.json { render json: @shukan.errors, status: :unprocessable_entity }
+      @status = false
     end
   end
 
   # DELETE /shukans/1 or /shukans/1.json
   def destroy
     @shukan.destroy
-    respond_to do |format|
-      format.html { redirect_to shukans_url, notice: "Shukan was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_shukan
-      @shukan = Shukan.find(params[:id])
+      @shukan = current_user.shukan.find_by(params[:id])
+      # @goalがないときgoals_urlにリダイレクト
+      redirect_to(goals_url, alert: "ERROR!!") if @goal.blank?
     end
 
     # Only allow a list of trusted parameters through.
