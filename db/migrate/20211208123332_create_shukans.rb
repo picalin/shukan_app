d35a2219ea777af516b798a297133babcc766215
picalin <s.hikaru.2125@gmail.com>
2021-12-08 21:44:54 +0900

    db migrated

--- db/migrate/20211208123332_create_shukans.rb
+++ db/migrate/20211208123332_create_shukans.rb
@@ -2,7 +2,7 @@ class CreateShukans < ActiveRecord::Migration[5.2]
   def change
     create_table :shukans do |t|
       t.string :title
-      t.done :position
+      t.integer :position
       t.references :user, foreign_key: true
       t.boolean :done, null: false, default: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_135434) do
+ActiveRecord::Schema.define(version: 2021_12_08_123332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shukans", force: :cascade do |t|
+    t.string "title"
+    t.integer "position"
+    t.bigint "user_id"
+    t.boolean "done", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shukans_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nickname", null: false
@@ -32,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_12_01_135434) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shukans", "users"
 end
