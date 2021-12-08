05bf67451b49f49228a8af2a73e3e0ce20550e6a
picalin <s.hikaru.2125@gmail.com>
2021-12-08 22:40:02 +0900

    updated shukans_model

--- app/models/shukan.rb
+++ app/models/shukan.rb
@@ -1,3 +1,15 @@
 class Shukan < ApplicationRecord
   belongs_to :user
+  acts_as_list scope: [:user_id]
+
+  validates :position, numericality: { only_integer: true, allow_nil: true }
+  validates :title, presence: true
+  validates :done, inclusion: { in: [true, false] }
+  after_initialize :set_default, if: :new_record?
+
+  private
+  
+  def set_default
+    self.done = false if self.done.blank?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :shukans, dependent: :destroy
+  has_many :shukans, -> { order("done ASC, position ASC") }, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
